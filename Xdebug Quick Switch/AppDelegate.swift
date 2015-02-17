05f164778a18901eecc9c48060a04c6e928de231
Jaesin Mulenex <Jaesin@users.noreply.github.com>
2015-02-17 13:18:56 -0800

    Initial Commit

---
+++ Xdebug Quick Switch/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Xdebug Quick Switch
+//
+//  Created by Jaesin Mulenex on 2/17/15.
+//  Copyright (c) 2015 Jaesin Mulenex. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Xdebug Quick SwitchTests/Xdebug_Quick_SwitchTests.swift
@@ -0,0 +1,36 @@
+//
+//  Xdebug_Quick_SwitchTests.swift
+//  Xdebug Quick SwitchTests
+//
+//  Created by Jaesin Mulenex on 2/17/15.
+//  Copyright (c) 2015 Jaesin Mulenex. All rights reserved.
+//
+
+import Cocoa
+import XCTest
+
+class Xdebug_Quick_SwitchTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
