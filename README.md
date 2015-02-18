54e775605d0545df676f20efacd57a832e82d9a4
Jaesin Mulenex <Jaesin@users.noreply.github.com>
2015-02-18 11:40:19 -0800

    Initial commit.

---
+++ README.md
@@ -0,0 +1,4 @@
+Xdebug Quick Switch
+===================
+
+This is a simple Mac status bar application to enable/disable xdebug. Even loading the xdebug php module can result in as much as a 50% performance hit.
--- Xdebug Quick Switch/AppDelegate.swift
+++ Xdebug Quick Switch/AppDelegate.swift
@@ -12,16 +12,70 @@ import Cocoa
 class AppDelegate: NSObject, NSApplicationDelegate {
 
     @IBOutlet weak var window: NSWindow!
+    @IBOutlet weak var statusMenu: NSMenu!
 
+    let statusItem = NSStatusBar.systemStatusBar().statusItemWithLength(-1)
+    let ioManager = NSFileManager.defaultManager()
 
+    let enabledFullPath = "/usr/local/etc/php/5.5/conf.d/ext-xdebug.ini"
+    let disabledFullPath = "/usr/local/etc/php/5.5/conf.d/ext-xdebug.ini.disabled"
+    
     func applicationDidFinishLaunching(aNotification: NSNotification) {
-        // Insert code here to initialize your application
+        let icon = NSImage(named: "statusIcon")
+//        icon.setTemplate(true)
+        statusItem.image = icon
+        statusItem.menu = statusMenu
+        
+        
     }
 
     func applicationWillTerminate(aNotification: NSNotification) {
         // Insert code here to tear down your application
     }
 
-
+    @IBAction func quitMenuItemClicked(sender: NSMenuItem) {
+        // Exit the utility.
+        NSApplication.sharedApplication().terminate(self)
+    }
+    @IBAction func enableMenuItemClicked(sender: NSMenuItem) {
+        
+        if(ioManager.fileExistsAtPath(disabledFullPath)){
+            let task1 = NSTask()
+            
+            task1.launchPath = "/bin/mv"
+            
+            task1.arguments = [disabledFullPath, enabledFullPath]
+            task1.launch()
+            task1.waitUntilExit()
+            
+            let task2 = NSTask()
+            
+            task2.launchPath = "/usr/bin/sudo"
+            
+            task2.arguments = ["/usr/sbin/apachectl", "-k", "graceful"]
+            task2.launch()
+            task2.waitUntilExit()
+        }
+    }
+    @IBAction func disableMenuItemClicked(sender: NSMenuItem) {
+        if(ioManager.fileExistsAtPath(enabledFullPath)){
+        
+            let task1 = NSTask()
+            
+            task1.launchPath = "/bin/mv"
+            
+            task1.arguments = [enabledFullPath, disabledFullPath]
+            task1.launch()
+            task1.waitUntilExit()
+            
+            let task2 = NSTask()
+            
+            task2.launchPath = "/usr/bin/sudo"
+            
+            task2.arguments = ["/usr/sbin/apachectl", "-k", "graceful"]
+            task2.launch()
+            task2.waitUntilExit()
+        }
+    }
 }
 
